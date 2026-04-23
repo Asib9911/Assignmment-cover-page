b7a08043d50c10f79cecffbd7126af164b7a1931
Asib9911 <asibur9911@gmail.com>
2026-04-23 17:28:40 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function downloadPDF() {
+    const element = document.getElementById("cover-page");
+
+    html2pdf()
+        .set({
+            margin: 0.5,
+            filename: 'NSTU-Cover-Page.pdf',
+            image: { type: 'jpeg', quality: 0.98 },
+            html2canvas: { scale: 2 },
+            jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' }
+        })
+        .from(element)
+        .save();
+}
